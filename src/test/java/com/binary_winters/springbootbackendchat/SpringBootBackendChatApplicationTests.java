726c2a6b52224e9d273e4a9b944cbaf1766f0b43
chipo <pnapoli33@gmail.com>
2020-12-02 00:09:25 -0300

    Initial commit with mongodb dependency commented out

---
+++ src/test/java/com/binary_winters/springbootbackendchat/SpringBootBackendChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binary_winters.springbootbackendchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackendChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
